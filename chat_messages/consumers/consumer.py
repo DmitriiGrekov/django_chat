a24720ca954d698945e6f00ab3b82ff59d1975d4
Греков Дмитрий Викторович <DGrekov@IBS.RU>
2023-11-12 15:08:38 +0300

    поправил ошибку

--- chat_messages/consumers/consumer.py
+++ chat_messages/consumers/consumer.py
@@ -35,5 +35,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def chat_message(self, event):
         message = event['message']
 
+        await self.send(text_data=json.dumps({
             'message': message
         }))
